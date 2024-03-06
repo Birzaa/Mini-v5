675178b90647107738aebcf97ac1470a98ca929e
Aurelien Bougrainville <abougrai@bess-f3r3s6.clusters.42paris.fr>
2024-03-06 16:35:42 +0100

    capart

--- builtins/cd.c
+++ builtins/cd.c
@@ -1,47 +1,63 @@
 #include "minishell.h"
 
-/* char *replace_tild_by_home_value(char *cmd, char *home)
+int	ft_more_than_one_char(char *str, char c)
 {
-	char *cpy =NULL;
-	int i = 0;
-	int check = 0;
-	while (cmd[i])
-	{
-		if (cmd[i] == '~' && check == 1)
-			return (NULL);
-		if (cmd[i] == '~')
-		{
-			cpy = home;
-			check = 1;
-		}
-		i++;
-		
-	}
-	return (cmd[1])
-} */
+	int	i;
+	int	more;
 
+	i = 0;
+	more = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			more++;
+		else if (more > 1)
+			return (1);
+		i++;
+	}
+	return (0);
+}
 void	ft_cd_tild(char *command, t_data *data)
 {
-
 	char	*new_cmd;
 	char	*tmp;
 	char	*home;
+	int		i;
+
 	home = NULL;
-	new_cmd = NULL;
-	(void)command;
-	(void)data;
-	(void)home;
-	(void)new_cmd;
-	(void)tmp;
+	new_cmd = "";
+	if (ft_more_than_one_char(command, '~'))
+	{
+		printf("bash: cd: %s: No such file or directory\n", command);
+		return ;
+		// g_ret_value = 1;
+	}
 	if ((ft_is_home_set(data->env)))
 	{
 		home = ft_getenv(data->env, "HOME");
 		if (!home)
 			return ;
-		printf("path : %s\n",ft_strjoin(home, &command[1]));
-	/* 	else if (chdir(&new_cmd))
+		i = 0;
+		while (command[i])
+		{
+			if (command[i] == '~')
+			{
+				while (command[i] == '~')
+				{
+					i++;
+					tmp = ft_strjoin(&home[5], &command[i]);
+					new_cmd = ft_strjoin(new_cmd, tmp);
+				}
+			}
+			else
+				i++;
+		}
+		printf("end join path : %s\n", new_cmd);
+		if (chdir(new_cmd))
 		{
 			printf("cd: %s: No such file or directory\n", command);
+			free(new_cmd);
+			return ;
 			// g_ret_value = 1;
 		}
 		free(new_cmd);
@@ -61,7 +77,7 @@ void	ft_cd_home(char *command, t_data *data)
 			return ;
 		else if (chdir(&home[5]))
 		{
-			printf("cd: %s: No such file or directory\n", command);
+			printf("cd: %s: No such file or directory\n", ft_strjoin(&home[5], command));
 			// g_ret_value = 1;
 		}
 		free(home);
