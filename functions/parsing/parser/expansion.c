050c7ddf05625021872bffb6b20d645963c7b92f
Aurelien Bougrainville <abougrai@bess-f3r3s4.clusters.42paris.fr>
2024-03-21 15:30:13 +0100

    capart

--- functions/parsing/parser/expansion.c
+++ functions/parsing/parser/expansion.c
@@ -6,7 +6,7 @@
 /*   By: abougrai <abougrai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 16:27:16 by thenwood          #+#    #+#             */
-/*   Updated: 2024/03/19 20:12:52 by abougrai         ###   ########.fr       */
+/*   Updated: 2024/03/21 15:26:43 by abougrai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ int	ft_expand_symbol(t_env *env, t_cmd_word *cmd)
 	{
 		if (tmp_join)
 			cmd->content = tmp_join;
-		return (perror(""), 0);
+		return (0);
 	}
 	tmp_full = ft_expand_symbol_bis(env, cmd, tmp_expand);
 	if (!tmp_full && !tmp_join)
@@ -111,6 +111,7 @@ void	expand(t_cmd_word *cmd, t_data *data)
 		cmd->content = "";
 		return ;
 	}
+	printf("test 3bisbis : si on veut expand symbol\n");
 	ft_expand_symbol(data->env, cmd);
 }
 
@@ -118,30 +119,56 @@ void	parsing_expand(t_cmd *cmd, t_data *data)
 {
 	t_cmd		*head;
 	t_cmd_word	*tmp_word;
+	int			check;
 
+	check = 0;
 	head = cmd;
 	while (head)
 	{
 		tmp_word = head->words;
 		while (tmp_word)
 		{
-			if (tmp_word->type == ENV && tmp_word->next && tmp_word->next->type == QOUTE)
+			if (tmp_word->type == ENV && (ft_check_symbol(tmp_word->next->content)))
 			{
-				printf("test1\n");
+				printf("test 1: si symbole apres un $\n");
+				printf("next content : %s\n",tmp_word->next->content );
+				tmp_word->type = WORD;
+				tmp_word = tmp_word->next;
+			}
+			if (tmp_word->type == ENV && tmp_word->next
+				&& tmp_word->next->type == QOUTE)
+			{
+				printf("test 2 : si ' apres nn $ \n");
 				tmp_word->content = "";
 			}
 			else if (tmp_word->type == ENV && tmp_word->state != 1
 				&& tmp_word->next)
 			{
-				printf("test2\n");
+				printf("test 3: si on veut expand\n");
 				tmp_word->type = WHITE_SPACE;
+				if (check)
+				{
+					printf("test 3bis: si on veut mutliple expand\n");
+					tmp_word->type = WORD;
+				}
 				tmp_word->content = "";
 				expand(tmp_word, data);
+				if (tmp_word->next && tmp_word->next)
+					tmp_word = tmp_word->next;
+				check = 1;
 			}
-			else if (tmp_word->type == ENV && (tmp_word->state == 1 || !tmp_word->next || tmp_word->next))
+			else if (tmp_word->type == ENV && (tmp_word->state == 1
+					|| !tmp_word->next || tmp_word->next))
 			{
-				printf("test3\n");
+				printf("test 4 : si on veut que $ devienne un mot \n");
 				tmp_word->type = WORD;
+			}
+			if (tmp_word->next && tmp_word->next->type == ENV && check)
+			{
+				printf("test 5 : si $ apres une expand\n");
+				check = 1;
+				tmp_word = tmp_word->next;
+				continue ;
 			}
 			tmp_word = tmp_word->next;
 		}
