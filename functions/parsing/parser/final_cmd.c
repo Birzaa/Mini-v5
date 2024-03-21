9749a38d272651f3ff84731222384855162a97f1
Thomas Henwood <thenwood@bess-f3r4s5.clusters.42paris.fr>
2024-03-21 19:09:58 +0100

    FFFFFFFFFFFFFFFFFFFFFFFf

--- functions/parsing/parser/final_cmd.c
+++ functions/parsing/parser/final_cmd.c
@@ -6,7 +6,7 @@
 /*   By: thenwood <thenwood@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 16:19:26 by thenwood          #+#    #+#             */
-/*   Updated: 2024/03/21 18:39:17 by thenwood         ###   ########.fr       */
+/*   Updated: 2024/03/21 19:06:54 by thenwood         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,7 +96,7 @@ int	init_parse(t_data *data)
 	data->parsed_cmd = parse(data->cmd);
 	/* return (0);
 } */
-	// print_list(data->lex);
+	print_list(data->lex);
 	// print_cmd_list(data->cmd);
 	// print_parsed_cmd(data->parsed_cmd);
 	return (0);
--- functions/parsing/parser/status.c
+++ functions/parsing/parser/status.c
@@ -1,30 +1,5 @@
 #include "minishell.h"
 
-int	ft_strstr(const char *big, const char *little, size_t len)
-{
-	size_t	i;
-	size_t	j;
-
-	if (!big && !len)
-		return (0);
-	i = 0;
-	if (little[i] == '\0')
-		return (0);
-	while (big[i])
-	{
-		j = 0;
-		while (big[i + j] == little[j] && i < len)
-		{
-			if (little[j + 1] == '\0' && j + 1 == ft_strlen(little) && (i
-					+ j) < len)
-				return (1);
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
-
 void	parsing_status(t_stack *list)
 {
 	t_node	*node;
@@ -38,6 +13,7 @@ void	parsing_status(t_stack *list)
 		{
 			if (ft_strncmp(node->next->content, "?", 1) == 0)
 			{
+				printf("ww: %s\n", node->next->content);
 				node->type = WHITE_SPACE;
 				node->next->type = WORD;
 				char *r = "55829651";
