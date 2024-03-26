1cfd42bd4ce922132e3354689c61d5982334f977
Ray <mrrayeku@gmail.com>
2024-03-26 18:53:26 +0100

    capart

--- functions/signal/signal.c
+++ functions/signal/signal.c
@@ -6,7 +6,7 @@
 /*   By: abougrai <abougrai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 02:45:57 by abougrai          #+#    #+#             */
-/*   Updated: 2024/03/26 18:43:30 by abougrai         ###   ########.fr       */
+/*   Updated: 2024/03/26 18:53:18 by abougrai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,11 @@ void	exec_signal(int sig)
 	if (sig == SIGQUIT)
 	{
 		ft_putstr_fd("Quit: (core dumped)\n", 2);
-		g_sig.sigquit = 1;
 		g_sig.status = 131;
 	}
 	else if (sig == SIGINT)
 	{
 		ft_putstr_fd("\n", 2);
-		g_sig.sigint = 1;
 		g_sig.status = 130;
 	}
 }
